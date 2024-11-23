e47279e61b736c80af75fd84c710955d15d99faf
tharunm1 <maddelatharun2@gmail.com>
2024-11-23 12:47:55 +0530

    updated base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:4000/api/v1";
+// export const BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://book-store-w11q.onrender.com/api/v1";
